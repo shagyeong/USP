f53eea1ee14eedf3c27806c8411a1a97168eb84c
shagyeong <shagyeong21@gmail.com>
2022-08-10 13:02:31 +0900

    shell update

---
+++ shell/00_hello.sh
@@ -0,0 +1,6 @@
+#!bin/sh
+echo "사용자 이름 : " $USER
+echo "홈 디렉토리 : " $HOME
+echo "hello!!"
+exit 0 
+
---
+++ shell/01_var.sh
@@ -0,0 +1,11 @@
+#!/bin/s
+v1="hi woo"
+echo $v1
+echo "$v1" #변수를 큰따옴표로 묶거나 묶지 않아도 된다
+echo '$v1' #작은따옴표로 묶어 문자로 취급
+echo \$v1  #역슬래시로 '$'을 문자로 취급
+
+echo "값 입력 : "
+read v1
+echo '$v1' = $v1
+exit 0
