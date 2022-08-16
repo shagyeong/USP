82958d10e85ea463ebe6a2e3b961fa33f6d57c46
shagyeong <shagyeong21@gmail.com>
2022-08-16 21:17:03 +0900

    ch01-029 update

---
+++ chapter01/029/hd.sh
@@ -0,0 +1,2 @@
+gcc -o hd hdump.c
+./hd sample.txt
---
+++ chapter01/029/hdump.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define BUFF 17
+#define ERR 01
+
+void usage(void);
+char* commandname;
+FILE* fp;
+
+void main(int argc, char* argv[]){
+    
+}
