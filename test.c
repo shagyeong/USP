6a9187ce233814352bb11b39ec57844e04225b5d
shagyeong <shagyeong21@gmail.com>
2024-03-25 14:52:17 +0900

    update: test

--- test.c
+++ test.c
@@ -5,13 +5,27 @@
 int main(void){
     DIR* dp;
     struct dirent* dent;
+    long loc;
 
     dp = opendir(".");
+    printf("start position : %ld\n", telldir(dp));
 
-    while((dent = readdir(dp))){
-        printf("name : %s ", (*dent).d_name);
-        printf("inode : %d\n", (int)((*dent).d_ino));
+    while(dent = readdir(dp)){
+        printf("read : %s -> ", (*dent).d_name);
+        printf("current position : %ld\n", telldir(dp));
     }
+
+    rewinddir(dp);
+    printf("rewinded position : %ld\n", telldir(dp));
+
+    /*항목 한 개를 읽은 후 오프셋의 위치*/
+    readdir(dp);
+    loc = telldir(dp);
+    seekdir(dp, loc);
+    printf("current position : %ld\n", telldir(dp));
+
+    dent = readdir(dp);
+    printf("read : %s\n", (*dent).d_name);
 
     closedir(dp);
 }
