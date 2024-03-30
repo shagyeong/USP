9f49d8c2462d43800bada7cf831150cbdf89dbdc
shagyeong <shagyeong21@gmail.com>
2024-03-30 23:22:04 +0900

    update: test

--- test.c
+++ test.c
@@ -4,15 +4,14 @@
 
 int main(void){
     struct stat statbuf;
-    int kind;
-    stat("linux.txt", &statbuf);
-
-    printf("mode = %d\n", (unsigned int)statbuf.st_mode);
     
-    if(S_ISLNK(statbuf.st_mode))
-        printf("LNK\n");
-    if(S_ISDIR(statbuf.st_mode))
-        printf("DIR\n");
-    if(S_ISREG(statbuf.st_mode))
-        printf("REG\n");
+    stat("linux.txt", &statbuf);
+    printf("mode = %o\n", (unsigned int)statbuf.st_mode);
+
+    if((statbuf.st_mode & __S_IREAD) != 0)
+        printf("rusr\n");
+    if((statbuf.st_mode & __S_IREAD >> 3) != 0)
+        printf("rgrp\n");
+    if((statbuf.st_mode & __S_IREAD >> 6) != 0)
+        printf("roth\n");
 }
