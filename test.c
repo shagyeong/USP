6e778cf99098bb55616564efc291f44676dfb81a
shagyeong <shagyeong21@gmail.com>
2024-03-30 23:00:31 +0900

    update: test

--- test.c
+++ test.c
@@ -1,18 +1,18 @@
 #include<sys/types.h>
 #include<sys/stat.h>
-#include<unistd.h>
 #include<stdio.h>
 
 int main(void){
     struct stat statbuf;
+    int kind;
     stat("linux.txt", &statbuf);
 
-    printf("inode = %d\n", (int)statbuf.st_ino);
     printf("mode = %d\n", (unsigned int)statbuf.st_mode);
-    printf("nlink = %d\n", (unsigned int)statbuf.st_nlink);
-    printf("uid = %d\n", (int)statbuf.st_uid);
-    printf("gid = %d\n", (int)statbuf.st_gid);
-    printf("size = %d\n", (int)statbuf.st_size);
-    printf("blksize = %d\n", (int)statbuf.st_blksize);
-    printf("blocks = %d\n", (int)statbuf.st_blocks);
+    
+    if(S_ISLNK(statbuf.st_mode))
+        printf("LNK\n");
+    if(S_ISDIR(statbuf.st_mode))
+        printf("DIR\n");
+    if(S_ISREG(statbuf.st_mode))
+        printf("REG\n");
 }
