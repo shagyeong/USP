1a0031f7771c020866f8e47d5591e81d92b3717c
shagyeong <shagyeong21@gmail.com>
2024-04-14 22:09:03 +0900

    update: test

--- test.c
+++ test.c
@@ -1,39 +1,20 @@
+#include<string.h>
+#include<unistd.h>
 #include<stdlib.h>
 #include<stdio.h>
 
 int main(void){
     FILE* fp;
-    int n;
-    long cur;
-    char buf[BUFSIZ];
-    if((fp = fopen("test.txt", "r")) == NULL){
-        perror("fopen");
-        exit(1);
-    }
+    int fd;
+    char template[32];
+    int ret;
 
-    cur = ftell(fp);
-    printf("current offset : %d\n", (int)cur);
-    n = fread(buf, sizeof(char), 5, fp);
-    buf[n] = '\0';
-    printf("str : %s\n", buf);
-
-    fseek(fp, 1, SEEK_CUR);
-    cur = ftell(fp);
-    printf("current offset : %d\n", (int)cur);
-    n = fread(buf, sizeof(char), 6, fp);
-    buf[n] = '\0';
-    printf("str : %s\n", buf);
-
-    fseek(fp, 1, SEEK_CUR);
-    cur = ftell(fp);
-    printf("current offset : %d\n", (int)cur);
-    n = fread(buf, sizeof(char), 11, fp);
-    buf[n] = '\0';
-    printf("str : %s\n", buf);
-
-    rewind(fp);
-    cur = ftell(fp);
-    printf("rewind offset : %d\n", (int)cur);
-
+    fp = tmpfile();
+    fpust("tmep file", fp);
     fclose(fp);
+
+    strcpy(template, "/tmp/testXXXXXX");
+    fd = mkstemp(template);
+    write(fd, "temp file", 10);
+    close(fd);
 }
