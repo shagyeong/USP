88c6e48afe51d3523897756bfbd34fc8260871e1
shagyeong <shagyeong21@gmail.com>
2024-04-25 17:28:41 +0900

    update: test

--- test.c
+++ test.c
@@ -1,1 +1,8 @@
-#
+#include<unistd.h>
+#include<stdio.h>
+
+int main(void){
+    printf("_PC_LINK_MAX : %ld\n", pathconf(".", _PC_LINK_MAX));
+    printf("_PC_NAME_MAX : %ld\n", pathconf(".", _PC_NAME_MAX));
+    printf("_PC_PATH_MAX : %ld\n", pathconf(".", _PC_PATH_MAX));
+}
