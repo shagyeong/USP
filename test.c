7265f31ed0992ec46cb1e7b775851db2eb135c81
shagyeong <shagyeong21@gmail.com>
2024-04-26 16:18:51 +0900

    update: test

--- test.c
+++ test.c
@@ -1,8 +1,13 @@
-#include<unistd.h>
+#include<sys/types.h>
+#include<utmp.h>
 #include<stdio.h>
 
 int main(void){
-    printf("_PC_LINK_MAX : %ld\n", pathconf(".", _PC_LINK_MAX));
-    printf("_PC_NAME_MAX : %ld\n", pathconf(".", _PC_NAME_MAX));
-    printf("_PC_PATH_MAX : %ld\n", pathconf(".", _PC_PATH_MAX));
+    struct utmp* ut;
+    
+    while((ut = getutent()) != NULL){
+        if(ut->ut_type != USER_PROCESS)
+            continue;
+        printf("%s %s\n", ut->ut_user, ut->ut_line);
+    }
 }
