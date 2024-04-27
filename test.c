afa873967277b1455f5f123a4bc0a39dee7333dc
shagyeong <shagyeong21@gmail.com>
2024-04-27 22:51:51 +0900

    update: test

--- test.c
+++ test.c
@@ -1,13 +1,9 @@
 #include<sys/types.h>
-#include<utmp.h>
+#include<time.h>
 #include<stdio.h>
 
 int main(void){
-    struct utmp* ut;
-    
-    while((ut = getutent()) != NULL){
-        if(ut->ut_type != USER_PROCESS)
-            continue;
-        printf("%s %s\n", ut->ut_user, ut->ut_line);
-    }
+    time_t tloc;
+    time(&tloc);
+    printf("time : %d\n", (int)tloc);
 }
