e2d38030e15b5bceeb6f16dab5d39021897eb8d4
shagyeong <shagyeong21@gmail.com>
2024-04-29 13:18:47 +0900

    update: test

--- test.c
+++ test.c
@@ -1,21 +1,11 @@
-#include<time.h>
+#include<sys/sysinfo.h>
 #include<stdio.h>
 
-char* op[] = {"%x %X", "%G년 %m월 %d일 %U주 %H:%M", "%r"};
-
 int main(void){
-    struct tm* tm;
-    int n;
-    time_t timep;
-    char buf[257];
+    struct sysinfo info;
+    sysinfo(&info);
 
-    time(&timep);
-    tm = localtime(&timep);
-
-    strftime(buf, sizeof(buf), op[0], tm);
-    printf("%s\n", buf);
-    strftime(buf, sizeof(buf), op[1], tm);
-    printf("%s\n", buf);
-    strftime(buf, sizeof(buf), op[2], tm);
-    printf("%s\n", buf);
+    printf("totalram : %ld\n", info.totalram);
+    printf("freeram : %ld\n", info.freeram);
+    printf("procs : %d\n", info.procs);
 }
