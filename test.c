b2fb083a6b2c2e47258db7198ac0d502f2752347
shagyeong <shagyeong21@gmail.com>
2024-06-10 14:26:06 +0900

    update: test

--- test.c
+++ test.c
@@ -1,24 +1,22 @@
+#include<sys/ucontext.h>
 #include<unistd.h>
 #include<sys/signal.h>
 #include<stdlib.h>
 #include<stdio.h>
 
-void sighandler(int signo){
-    psignal(signo, "received signal:");
-    sleep(5);
-    printf("in signal handler, after sleep\n");
+void sighandler(int signo, siginfo_t* sf, ucontext_t* uc){
+    psiginfo(sf, "received signal:");
+    printf("si_code : %d\n", sf->si_code);
 }
 int main(void){
     struct sigaction act;
+    act.sa_flags = SA_SIGINFO;
+    act.sa_sigaction = (void (*)(int, siginfo_t *, void *))sighandler;
     sigemptyset(&act.sa_mask);
-    sigaddset(&act.sa_mask, SIGQUIT); //SIGQUIT 블로킹
-    act.sa_flags = 0;
-    act.sa_handler = sighandler;
-    if(sigaction(SIGINT, &act, (struct sigaction *)NULL) < 0){
+    if(sigaction(SIGUSR1, &act, (struct sigaction*) NULL) < 0){
         perror("sigaction");
+        perror("sigactioin");
         exit(1);
     }
-    fprintf(stderr, "input SIGINT: ");
     pause();
-    fprintf(stderr, "after signal handler\n");
 }
