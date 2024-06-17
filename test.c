93000aabd6efd673cf85ef3806ea2501d9e4ad3a
shagyeong <shagyeong21@gmail.com>
2024-06-18 00:47:25 +0900

    update: test

--- test.c
+++ test.c
@@ -10,17 +10,52 @@ void printbinary(int integer){
     }
     printf("\n");
 }
+int returnmask(char* ugo, char* rwx){
+    int mask = 0B0000000000000000;
+    if(strcmp(rwx, 'x') == 0){mask +=0B001;}
+    else if(strcmp(rwx, 'w') == 0){mask +=0B010;}
+    else if(strcmp(rwx, 'wx') == 0){mask +=0B011;}
+    else if(strcmp(rwx, 'r') == 0){mask +=0B100;}
+    else if(strcmp(rwx, 'rx') == 0){mask +=0B101;}
+    else if(strcmp(rwx, 'rw') == 0){mask +=0B110;}
+    else if(strcmp(rwx, 'rwx') == 0){mask +=0B111;}
+    
+    
+}
 
 int main(int argc, char* argv[]){
-    char* fname = argv[(int)strlen(*argv)];
+    char* fname = argv[argc - 1];
     struct stat statbuf; stat(fname, &statbuf);
     int mode = statbuf.st_mode;
     
+    int index1 = 0;
+    int index2 = 0;
+
+    char sign; //+-=
     char ugo[3];
     char rwx[3];
 
-    for(int i = 0; i < (int)strlen(*argv); i++){
-        printf("%s ", argv[i]);
+    for(int i = 1; i < argc - 1; i++){
+        while(argv[i][index1] != '+' && argv[i][index1] != '-'){
+            ugo[index2] = argv[i][index1];
+            index1++;
+            index2++;
+        }
+        printf("%s", ugo);
+        sign = argv[i][index1];
+        index1++;
+        index2 = 0;
+        while(argv[i][index1] != '\0'){
+            rwx[index2] = argv[i][index1];
+            index1++;
+            index2++;
+        }
+        
+        printf("%c %s %s\n", sign, ugo, rwx);
+        ugo[0] = '\0';ugo[1] = '\0';ugo[2] = '\0';
+        rwx[0] = '\0';rwx[1] = '\0';rwx[2] = '\0';
+        index1 = 0;
+        index2 = 0;
     }
 }
 
