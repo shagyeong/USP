28d3991978cc19f9e2d564fca212650005331c5e
shagyeong <shagyeong21@gmail.com>
2024-06-19 14:50:25 +0900

    update: test

--- test.c
+++ test.c
@@ -2,7 +2,15 @@
 //심벌릭 링크 파일의 내용과 원본 파일의 경로를 출력하는 프로그램을 작성하시오.
 //$ usrsym test.c
 #include<stdio.h>
+#include<unistd.h>
+#include<limits.h>
+#include<stdlib.h>
 
 int main(int argc, char* argv[]){
-    readlink
+    if(symlink(argv[1], "test.sym") == -1){
+        perror("symlink");
+    }
+    char buf[BUFSIZ];
+    readlink("test.sym", buf, BUFSIZ); printf("%s\n", buf);
+    realpath("test.sym", buf); printf("%s\n", buf);
 }
